b18d3fec50196bda98778a7960c648d93357ff79
Kanagaraj-R <kanagarajram88@gmail.com>
2023-06-11 08:58:41 +0530

    commit

---
+++ DAY-5 -TASK/resume.js
@@ -0,0 +1,36 @@
+const resumeData = {
+    "name": "Kanagaraj R",
+    "title": "Emerging - Full Stack Developer ",
+    "contact": {
+        "email": "Kanagarajram88@gmail.com",
+        "phone": "6374203980",
+        "address": "Erode,Tamilnadu"
+    },
+    "education": [
+        {
+            "degree": "Bachelor of computer Application",
+            "major": "Computer Science",
+            "university": "Dr SNS Rajalakshmi College Of Arts And Science ",
+            "year": 2016
+        },
+    ],
+    "courses": [
+        {
+            "title": "Full Stack Developement",
+            "company": "GUVI",
+            "location": "Chennai,Tamilnadu",
+            "duration": "3 Months",
+            "responsibilities": [
+                "Learned Javascript fron scratch",
+                "Good Knowledge in web developement",
+                "Codekata,Webkata"
+            ]
+        },
+    ],
+    "skills": ["JavaScript", "HTML", "CSS", "Java", "Sql"]
+};
+
+// Accessing resume data
+console.log(resumeData);
+
+
